35c2f81789716ac5f7e0845f0029ab2bc3f8e9e4
george_wsl <g3orrg301@gmail.com>
2024-05-15 20:15:01 +0300

    Test tesseract from static image

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from "react";
+import { createWorker } from "tesseract.js";
 
 export default function Home() {
+  const [myData, setMyData] = useState('');
+
+  const handleClick = async () => {
+    const worker = await createWorker('eng');
+    const ret = await worker.recognize('/scanMe.png');
+    console.log(ret.data.text)
+    setMyData(ret.data.text);
+    await worker.terminate();
+  }
+
   return (
     <main>
-      Project here
+      <button onClick={handleClick}>Click me</button>
+      <div>{myData}</div>
     </main>
   );
 }
